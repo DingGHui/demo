087ee274911fff95a3d48f4bb46a97a71d210c84
DingGuangHui <guanghui.ding@jiamiantech.com>
2020-08-28 14:40:06 +0800

    添加wx download

---
+++ src/test/java/com/ding/demo/Main.java
@@ -0,0 +1,20 @@
+package com.ding.demo;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * @author ding
+ * @date 2020/8/6
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        HashSet<Object> objects = new HashSet<>();
+        objects.add("1");
+
+        int i = "s".hashCode();
+    }
+}
