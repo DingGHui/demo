93d44f4230972caf0814bd6329cbed10ee11bfa1
ding <785508803@qq.com>
2020-11-14 11:21:23 +0800

    初始化

---
+++ src/main/java/com/ding/demo/client/DemoClientHandler.java
@@ -0,0 +1,15 @@
+package com.ding.demo.client;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+
+/**
+ * 客户端业务逻辑
+ */
+public class DemoClientHandler extends SimpleChannelInboundHandler {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("Server say : " + msg.toString());
+    }
+}
+
